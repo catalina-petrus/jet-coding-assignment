99e6622f3c03039aa5fa4f9a27119f5f8e602888
Catalina Antonia Petrus <c.petrus@student.utwente.nl>
2025-04-02 23:24:57 +0200

    Added NavigationBar Added postcode labels and insert&search directly -UI changes in address and Rating position

---
+++ assignment-Jet/src/components/NavigationBar.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { User } from 'lucide-vue-next'
+</script>
+
+<template>
+  <nav class="w-full bg-[#FF8000] text-white shadow-md px-6 py-3 flex justify-between items-center">
+    <div class="text-lg font-bold">
+        <img src="@/assets/logo.png" alt="Logo" class="w-10 h-10">
+    </div>
+    <div class="text-lg font-bold">Coding Assignment Early Careers Just Eat Takeaway</div>
+    <div class="flex items-center gap-2">
+      <span class="text-sm font-medium">Cătălina Antonia Petruș</span>
+      <User class="w-5 h-5 text-white" />
+    </div>
+  </nav>
+</template>
